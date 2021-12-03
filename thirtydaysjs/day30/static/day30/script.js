926366e29e8c50b2c48c1fea71404156fa13fd71
smena8 <puttinonthefritz@protonmail.com>
2021-12-03 13:19:51 -0800

    day 30 javascript quiz, needs lots of cleanup

---
+++ thirtydaysjs/day30/static/day30/script.js
@@ -0,0 +1,515 @@
+let studyGuide = { english : {
+                                truefalse:[
+  {
+    question: '\"Renaissance\" means \"rebirth.\"',
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "The Wycliffe translation of the Bible was significant and allowed people to read the Bible for themselves.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Tyndale translated the Bible from Hebrew and Greek texts.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "The King James Version of the Bible was translated mainly from existing English texts.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "King James did not actually do any of the translating for the King James Version.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Francis Bacon wrote many essays.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "The Pilgrim's Progress is an allegory about Christian's journey to heaven.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Shakespeare wrote around 154 sonnets.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Shakespeare wrote around 38 plays.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Shakespeare's company was responsible for building the famous playhouse called the Globe Theatre.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Macbeth is a play about greed, a quest for power, and guilt.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Jonathan Swift is known as one of England's greatest satirists.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Satire ridicules behaviors or institutions for the purpose of improving society.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Jonathan Swift encouraged the Irish to resist oppression.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Verbal irony occurs when what is said is not exactly what is meant.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Jonathan Swift's proposal of using infants for food and other uses was not meant seriously, but was satirical.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Alexander Pope wrote a mock-epic that stemmed from a real incident.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Jane Austen criticized social conventions of her time.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Pride and Prejudice is about a family of five daughters and their quest for marriage.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Frankenstein is a gothic novel because of its elements of mystery, horror, and the supernatural.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "The \"Victorian Age\" was named for Queen Victoria.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Charles Dickens pushed for social reform.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "The Tale of Two Cities tells a story of London and Paris.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "George Bernard Shaw was a socialist.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "George Bernard Shaw's play, Pygmalion, portrays the issue of class distinction.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "George Orwell is the pen name of Eric Blair.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+  {
+    question: "Animal Farm is a political satire about oppressive governments like that of communist Russia.",
+    answers: {
+      True: 'True',
+      False: 'False',
+    },
+    correctAnswer: 'True'
+  },
+],
+                                matchingSetOne: {
+                                questions: [{
+                                    index: 0,
+                                    text: "William Shakespeare"
+                                  }, {
+                                    index: 1,
+                                    text: "Jonathan Swift"
+                                  }, {
+                                    index: 2,
+                                    text: "Alexander Pope"
+                                  }, {
+                                    index: 3,
+                                    text: "John Bunyan"
+                                  }, {
+                                    index: 4,
+                                    text: "John Keats"
+                                  }, {
+                                    index: 5,
+                                    text: "Lord Byron"
+                                  }, {
+                                    index: 6,
+                                    text: "Percy Shelley"
+                                  }, {
+                                    index: 7,
+                                    text: "Samuel Coleridge"
+                                  }, {
+                                    index: 8,
+                                    text: "William Wordsworth"
+                                  }, {
+                                    index: 9,
+                                    text: "Jane Austen"
+                                  }, {
+                                    index: 10,
+                                    text: "Alfred Lord Tennyson"
+                                  }, {
+                                    index: 11,
+                                    text: "Robert Browning"
+                                  }, {
+                                    index: 12,
+                                    text: "Elizabeth B. Browning"
+                                  }, {
+                                  index: 13,
+                                    text: "Charles Dickens"
+                                  }, {
+                                    index: 14,
+                                    text: "Mary Shelley"
+                                  }, {
+                                    index: 15,
+                                    text: "George Bernard Shaw"
+                                  }
+                                ],
+                                answers: [{
+                                    index: 0,
+                                    text: "Macbeth"
+                                  }, {
+                                    index: 1,
+                                    text: "Modest Proposal"
+                                  }, {
+                                    index: 2,
+                                    text: "The Rape of the Lock"
+                                  }, {
+                                    index: 3,
+                                    text: "The Pilgrim's Progress"
+                                  }, {
+                                    index: 4,
+                                    text: "To Autumn."
+                                  }, {
+                                    index: 5,
+                                    text: "Don Juan"
+                                  }, {
+                                    index: 6,
+                                    text: "Ode to the West Wind"
+                                  }, {
+                                    index: 7,
+                                    text: "The Rime of the Ancient Mariner"
+                                  }, {
+                                    index: 8,
+                                    text: "Tintern Abbey"
+                                  }, {
+                                    index: 9,
+                                    text: "Pride and Prejudice"
+                                  }, {
+                                    index: 10,
+                                    text: "The Lady of Shalott"
+                                  }, {
+                                    index: 11,
+                                    text: "My Last Duchess"
+                                  }, {
+                                    index: 12,
+                                    text: "Sonnet 43"
+                                  }, {
+                                  index: 13,
+                                    text: "A Tale of Two Cities"
+                                  }, {
+                                    index: 14,
+                                    text: "Frankenstein"
+                                  }, {
+                                    index: 15,
+                                    text: "Pygmalion"
+                                  }
+                                ],
+                                correctAnswers: {
+                                      0: 0,
+                                      1: 1,
+                                      2: 2,
+                                      3: 3,
+                                      4: 4,
+                                      5: 5,
+                                      6: 6,
+                                      7: 7,
+                                      8: 8,
+                                      9: 9,
+                                      10: 10,
+                                      11: 11,
+                                      12: 12,
+                                      13: 13,
+                                      14: 14,
+                                      15: 15,
+                                },
+                              },
+                              }
+                  };
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    const container = document.querySelector('.main__container');
+
+
+    let score = 0;
+    let pointsPerQuestion =  100/(studyGuide.english.truefalse.length + studyGuide.english.matchingSetOne.questions.length)
+   currentIndex = 0;
+
+   const questionElement = document.querySelector('#question')
+   const topicElement = document.querySelector('#topic');
+   const choicesContainer = document.querySelector('#choices');
+
+   startQuiz();
+
+    function startQuiz() {
+        topicElement.textContent = 'English';
+        if (currentIndex === 0) {
+            trueFalseQuestions();
+        }
+    }
+
+    function trueFalseQuestions() {
+        const questionsTrueFalse = studyGuide.english.truefalse
+        const shuffledQuestions = questionsTrueFalse.sort(() => Math.random() - .5)
+        questionElement.textContent = shuffledQuestions[currentIndex].question;
+        let buttons = document.querySelectorAll('button');
+        buttons.forEach(btn => {
+            btn.addEventListener('click', function() {
+                btnClick(shuffledQuestions);
+            })
+        })
+
+    }
+
+    function btnClick(shuffledQuestions) {
+        let selectedAnswer = event.target.id;
+        if (shuffledQuestions[currentIndex]) {
+            if (selectedAnswer.toLowerCase() === shuffledQuestions[currentIndex].correctAnswer.toLowerCase()) {
+                score += pointsPerQuestion;
+            }
+        }
+        currentIndex += 1;
+        if (currentIndex < shuffledQuestions.length) {
+            questionElement.textContent = shuffledQuestions[currentIndex].question;
+        } else {
+            startMatchingSetOne();
+        }
+    }
+
+    function startMatchingSetOne() {
+        questionElement.textContent = 'Match the author to their work.';
+        const matchingSetOne = studyGuide.english.matchingSetOne;
+        const shuffledMatchSetQuestions = matchingSetOne.questions.sort(() => Math.random() - .5);
+        const shuffledMatchSetAnswers = matchingSetOne.answers.sort(() => Math.random() - .5);
+        createMatchElements(shuffledMatchSetQuestions, shuffledMatchSetAnswers);
+    }
+
+    function createMatchElements(questionsObjects, answersObjects) {
+
+        let questionDivs = '';
+        questionsObjects.forEach(string => {
+                questionDivs += `<div class="choice">
+                                    <button type="button" id="${string.text}" data-index="${string.index}" data-match="">${string.text}</button>
+                                </div>`
+        })
+
+        let centerDiv ='<div class="center"></div>';
+
+        let answerDivs = '';
+        answersObjects.forEach(string => {
+                answerDivs += `<div class="choice">
+                                    <button type="button" id="${string.text}" data-index="${string.index}" data-match="">${string.text}</button>
+                                </div>`
+        })
+
+        choicesContainer.innerHTML = '<div class="set-one">' + questionDivs + '</div>' + centerDiv + '<div class="set-two">' + answerDivs + '</div>'
+        styleButtonOnClick();
+    }
+
+    function styleButtonOnClick() {
+        buttons = document.querySelectorAll('button');
+        buttons.forEach(btn => {
+                    btn.addEventListener('click', function(event) {
+                        if (event.target.classList.contains('matched')) return
+                        let setName = event.target.parentElement.parentElement.classList[0]
+                        alreadySelected = document.querySelectorAll('.selected');
+                        for (i=0; i < alreadySelected.length; i++) {
+                            let selectedSetName = alreadySelected[i].parentElement.parentElement.classList[0];
+                            if (selectedSetName === setName) {
+                                alreadySelected[i].classList.remove('selected');
+                                event.target.classList.toggle('selected');
+                                event.target.blur();
+                                return
+                            }
+                            if (selectedSetName !== setName) {
+                                alreadySelected[i].classList.remove('selected');
+                                event.target.classList.remove('selected');
+                                alreadySelected[i].classList.add('matched');
+                                alreadySelected[i].dataset.match = event.target.dataset.index;
+                                event.target.classList.add('matched');
+                                event.target.dataset.match = alreadySelected[i].dataset.index;
+                                drawLine(event.target, alreadySelected[i]);
+                                if (checkComplete(buttons)) {
+                                    addMatchestoScore();
+                                    displayScore(score);
+                                };
+                                return
+                            }
+                        }
+                        event.target.classList.toggle('selected');
+                        event.target.blur();
+
+                    })
+            })
+    }
+
+    function drawLine(choice1, choice2) {
+        const centerDiv = document.querySelector('.center')
+        const centerTop = centerDiv.getBoundingClientRect().top
+        const centerHeight = centerDiv.offsetHeight
+        const choiceHeight = choice2.parentElement.clientHeight
+        const choice1Bottom = choice1.parentElement.getBoundingClientRect().bottom
+        const choice2Bottom = choice2.parentElement.getBoundingClientRect().bottom
+        const choiceSmall = (choice1.parentElement.getBoundingClientRect().bottom < choice2.parentElement.getBoundingClientRect().bottom) ? choice1.parentElement : choice2.parentElement
+        const choiceLarge = (choice1.parentElement.getBoundingClientRect().bottom > choice2.parentElement.getBoundingClientRect().bottom) ? choice1.parentElement : choice2.parentElement
+        const choiceBottomSmallest = choiceSmall.getBoundingClientRect().bottom
+        const choiceBottomLargest = choiceLarge.getBoundingClientRect().bottom
+        middleOfChoiceSmall = choiceSmall.clientHeight / 2
+        middleOfChoiceLarge = choiceLarge.clientHeight / 2
+
+        let newElement = document.createElement('div')
+        newElement.setAttribute('class', 'line');
+        newElement.style.top = (choiceBottomSmallest - centerTop) - (middleOfChoiceSmall) + 'px';
+        newElement.style.height = (choiceBottomLargest - choiceBottomSmallest) + 'px';
+        let classOfChoice1Parent = choice1.parentElement.parentElement.classList[0]
+        if (choiceSmall.parentElement.classList[0] === 'set-one') {
+            newElement.innerHTML = `<svg style='width: 100%; height: 100%;'>
+                                        <line x1="0" y1="0" x2="100%" y2="100%"
+                                            style="stroke:var(--light);stroke-width:2"/>
+                                    </svg>`
+        } else if (choiceSmall.parentElement.classList[0] === 'set-two') {
+            newElement.innerHTML = `<svg style='width: 100%; height: 100%;'>
+                                        <line x1="100%" y1="0" x2="0" y2="100%"
+                                            style="stroke:var(--light);stroke-width:2"/>
+                                    </svg>`
+        }
+        centerDiv.appendChild(newElement);
+    }
+
+    function checkComplete(buttons) {
+        return [...buttons].every(button => button.classList.contains('matched'))
+    }
+
+    function addMatchestoScore() {
+        buttonQuestions = document.querySelector('.set-one').querySelectorAll('button');
+        buttonQuestions.forEach(q => {
+            if (q.dataset.index === q.dataset.match) {
+                score += pointsPerQuestion
+            }
+        })
+    }
+
+    function displayScore(score) {
+        container.innerHTML = `<h1>Quiz complete! You scored ${Math.round(score)}%.</h1>`;
+    }
+
+
+
+});
+
